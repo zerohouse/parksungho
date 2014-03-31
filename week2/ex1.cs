b157efffa1e0169911a09a1f29e35f3e85163885
zerohouse <parksungho86@gmail.com>
2014-04-01 01:13:17 +0900

    re

---
+++ week2/ex1.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+namespace next {
+	class Ex3 {
+		static void Main(string[] args){
+			const int SIZE = 5;
+			int[] arr;
+			int i, sum;
+			arr = new int[SIZE];
+
+			i = 0;
+			while (i < SIZE) {
+				Console.Write("num[{0}]? ", i);
+				string cmd = Console.ReadLine();
+				arr[i] = Convert.ToInt32(cmd);
+				i = i + 1;
+			}
+				i=0;
+				sum=0;
+				while(i<SIZE){
+					sum = sum + arr[i];
+					i = i + 1;
+				}
+
+				Console.WriteLine("Sum={0}",sum);
+		}
+	}
+}
